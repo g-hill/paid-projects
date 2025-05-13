06b06cfbc4de4dc85ab3defa3f2ee2b63fc3e152
Geoff Hill <geoffhill@protonmail.com>
2025-05-13 12:24:05 -0400

    add topnav

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,14 +1,22 @@
-import { SidebarNav } from './SidebarNav'
-import { CardBoard } from './CardBoard'
-
+import { SidebarNav } from "./SidebarNav"
+import { CardBoard } from "./CardBoard"
+import { TopNav } from "./TopNav"
 
 export function DashboardLayout() {
   return (
-    <><div className="flex h-screen">
-            <SidebarNav /><main className="flex-1 p-6 overflow-auto bg-muted/40">
-        <CardBoard />
-      </main>
+    <div className="flex flex-col h-screen">
+      {/* Top Navigation */}
+      <header className="w-full border-b bg-background px-6 py-3 shadow-sm">
+        <TopNav />
+      </header>
+
+      {/* Content below the nav */}
+      <div className="flex flex-1 overflow-hidden">
+        <SidebarNav />
+        <main className="flex-1 p-6 overflow-auto bg-muted/40">
+          <CardBoard />
+        </main>
+      </div>
     </div>
-    </>
   )
 }
