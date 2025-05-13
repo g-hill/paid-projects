15f1e6e336da5f2ee0a857d087dd6e4059590c62
Geoff Hill <geoffhill@protonmail.com>
2025-05-13 09:39:06 -0400

    add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { AuthForm } from './components/AuthForm'
+import { ThemeProvider } from './components/ui/ThemeProvider'
 
 function App() {
   
   return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div className="min-h-screen flex items-center justify-center bg-muted">
       <AuthForm />
     </div>
+    </ThemeProvider>
   )
 }
 
